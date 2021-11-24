7d24e82eeeceab656eaa12103a41a91a8aa03268
PRITAM DAS <93383417+hacker-404-error@users.noreply.github.com>
2021-11-24 12:58:07 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,13 @@
+cout<<"\nEnter Roll No. :";
+    // getline(cin, s[i].roll);
+
+    // cout<<"\nEnter Class :";
+    // getline(cin, s[i].clas_s);
+
+    // cout<<"\nEnter Total Marks :";
+    // cin>>s[i].tot;
+
+    // cout<<"\nEnter Marks Obtained. :";
+    // cin>>s[i].obt;
+
+    // s[i].per = (s[i].obt / s[i].tot)*100;
